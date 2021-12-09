59c4155a52af5c1280e4fcf64a6db4004eda6d6d
Kyle Bernstein <kyle.bernstein@centro.net>
2021-12-09 15:09:08 -0500

    Temporarily disabled VRAM viewer

--- src/main.py
+++ src/main.py
@@ -10,20 +10,20 @@ from pyboy import PyBoy
 
 pyboy = PyBoy()
 main_window = pyglet.window.Window(SCREEN_WIDTH * DISPLAY_FACTOR, SCREEN_HEIGHT * DISPLAY_FACTOR, "PyBoy")
-vram_viewer = pyglet.window.Window(512, 512, "VRAM")
+# vram_viewer = pyglet.window.Window(512, 512, "VRAM")
 
 main_window.set_location(100, 100)
-vram_viewer.set_location(main_window.get_location()[0] + main_window.width + 10, main_window.get_location()[1] + 20)
+# vram_viewer.set_location(main_window.get_location()[0] + main_window.width + 10, main_window.get_location()[1] + 20)
 
 pyglet.gl.glClearColor(255, 255, 255, 0)
 
 fps_display = pyglet.window.FPSDisplay(window=main_window)
 
 pixels = []
-tile_pixels = {}
+# tile_pixels = {}
 
 batch = pyglet.graphics.Batch()
-tile_batch = pyglet.graphics.Batch()
+# tile_batch = pyglet.graphics.Batch()
 
 # Setup Screen - TODO move to function
 for i in range(SCREEN_WIDTH):
@@ -44,28 +44,28 @@ for i in range(SCREEN_WIDTH):
 
 
 # Setup VRAM Viewer - TODO move to function
-x = 0
-y = 0
+# x = 0
+# y = 0
 
-for tile in pyboy.get_tiles():
+# for tile in pyboy.get_tiles():
 
-    if x >= 512:
-        x = 0
-        y += 8 * 2
+#     if x >= 512:
+#         x = 0
+#         y += 8 * 2
 
-    for i in range(len(tile)):
-        line = tile[i]
-        for j in range(len(line)):
-            tile_pixels[(x + i), (y + j)] = shapes.Rectangle(
-                x=x + (j * 2),
-                y=512 - y - (i * 2),
-                width=2,
-                height=2,
-                color=(255, 0, 0),
-                batch=tile_batch
-            )
+#     for i in range(len(tile)):
+#         line = tile[i]
+#         for j in range(len(line)):
+#             tile_pixels[(x + i), (y + j)] = shapes.Rectangle(
+#                 x=x + (j * 2),
+#                 y=512 - y - (i * 2),
+#                 width=2,
+#                 height=2,
+#                 color=(255, 0, 0),
+#                 batch=tile_batch
+#             )
 
-    x += 8 * 2
+#     x += 8 * 2
 
 
 def run_frame(dt):
@@ -85,28 +85,28 @@ def run_frame(dt):
             if current_color[0] != red or current_color[1] != green or current_color[2] != blue:
                 pixels[row][col].color = (red, green, blue)
 
-    x = 0
-    y = 0
-    for tile in pyboy.get_tiles():
+    # x = 0
+    # y = 0
+    # for tile in pyboy.get_tiles():
 
-        if x >= 512:
-            x = 0
-            y += 16
+    #     if x >= 512:
+    #         x = 0
+    #         y += 16
 
-        for i in range(len(tile)):
-            line = tile[i]
-            for j in range(len(line)):
-                color = line[j]
-                red = color >> 16
-                green = (color >> 8) & 0xFF
-                blue = color & 0xFF
+    #     for i in range(len(tile)):
+    #         line = tile[i]
+    #         for j in range(len(line)):
+    #             color = line[j]
+    #             red = color >> 16
+    #             green = (color >> 8) & 0xFF
+    #             blue = color & 0xFF
 
-                current_color = tile_pixels[(x + i), (y + j)].color
+    #             current_color = tile_pixels[(x + i), (y + j)].color
 
-                if current_color[0] != red or current_color[1] != green or current_color[2] != blue:
-                    tile_pixels[(x + i), (y + j)].color = (red, green, blue)
+    #             if current_color[0] != red or current_color[1] != green or current_color[2] != blue:
+    #                 tile_pixels[(x + i), (y + j)].color = (red, green, blue)
 
-        x += 16
+    #     x += 16
 
 
 @main_window.event("on_draw")
@@ -116,10 +116,10 @@ def on_main_draw():
     fps_display.draw()
 
 
-@vram_viewer.event("on_draw")
-def on_vram_draw():
-    vram_viewer.clear()
-    tile_batch.draw()
+# @vram_viewer.event("on_draw")
+# def on_vram_draw():
+#     vram_viewer.clear()
+#     tile_batch.draw()
 
 
 pyglet.clock.schedule_interval(run_frame, 1 / 59.7275)
